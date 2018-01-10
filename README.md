4a8c3a776fd1ff89ac1a6b71c1285d25bf738b70
guillaumeAder1 <guillaume.ader1@gmail.com>
2018-01-10 19:14:14 +0000

    add profil page with or whitout store connection

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # movieFilter
 
 create and save filters for '''movies'''
+
+https://medium.com/@notrab/getting-started-with-create-react-app-redux-react-router-redux-thunk-d6a19259f71f
---
+++ src/containers/profile/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+class Profile extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Profile Page</h1>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+
+}, dispatch)
+
+// export default connect(
+//     mapStateToProps,
+//     mapDispatchToProps
+// )(Profile)
+
+export default Profile
